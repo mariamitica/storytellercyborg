9153ad1b8ae6c95b46796915d36372f26603f76b
Maria Mitica <maria.mitica@1and1.ro>
2021-12-14 12:44:55 +0200

    custom header

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const port = 3000;
 app.get('/', (req, res) => {
 	res.send('BEWARE!!');
 });
+
+app.get('/test', (_req, res) => {
+	res.status('418');
+	res.set('X-my-custom-header', 'mumu');
+})
 app.listen(port, () => console.log(`App is running on port ${port}`));
 

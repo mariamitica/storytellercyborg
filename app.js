e1dbbded92b09ca2e5447061b738d8b04c12a835
Maria Mitica <maria.mitica@1and1.ro>
2021-12-14 15:58:18 +0200

    custom header

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 app.get('/', (_req, res) => {
 	res.send('BEWAREsss!!');
 });
 
 app.get('/test', (_req, res) => {
-	res.status('418');
+	res.status(418);
 	res.set('X-my-custom-header', 'mumu');
 	res.send('ddd')
 })
